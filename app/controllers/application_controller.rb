0e4fe397e8b8dff6ddc48727897e67a0d6c934cd
Siddhartha Gudipati <me@websiddu.com>
2015-05-26 01:10:11 -0700

    Fixing cors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  # after_filter :cors_set_access_control_headers
+  #after_filter :cors_set_access_control_headers
 
   def cors_set_access_control_headers
     headers['Access-Control-Allow-Origin'] = '*'
